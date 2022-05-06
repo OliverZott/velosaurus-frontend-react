fc08d3654ffabe64cc233670883b565be7095d25
Oliver Zott <zott_oliver@web.de>
2022-05-06 18:07:18 +0200

    Initial commit

---
+++ src/entity/Tour.tsx
@@ -0,0 +1,12 @@
+export class Tour {
+
+    constructor(
+        public name: string = '',
+        public length: number = 0,
+        public date: string = '',
+        public temperatureC: number = 0,
+        public temperatureF: number = 0,
+        public summary: string = '',
+    ) { }
+
+}
---
+++ src/pages/Tours.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Tour } from "../entity/Tour";
+
+const Tours = () => {
+    const url = "https://localhost:7019/WeatherForecast";
+    const [tours, setTours] = useState<Tour[]>([]);
+
+
+    useEffect(() => {
+        (
+            async () => {
+                const { data } = await axios.get(url);
+                setTours(data);
+                console.log(data.data);
+                console.log(data);
+            }
+        )()
+    }, []);
+
+
+
+
+    const renderTour = () => {
+        return tours.map((tour: Tour) => {
+            return (
+                <tr key={tour.date}>
+                    <td>{tour.date}</td>
+                    <td>{tour.summary}</td>
+                    <td>
+                        {/* <div>
+                        <a href="#" className="btn btn-sm btn-outline-secondary" onClick={() => showDetails(tour.id)}>Show</a>
+                    </div> */}
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    return (
+        <div>
+            <h1>Tours</h1>
+
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Length</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTour()}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Tours;
